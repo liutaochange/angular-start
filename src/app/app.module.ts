cb1d71ac7ee4904e5f01d8bef37d44c3bb135f58
liutaochange <liutaochange@163.com>
2018-06-04 11:32:15 +0800

    hero complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,14 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
   declarations: [
@@ -14,12 +20,22 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeroSearchComponent
   ],
   imports: [
+    // 浏览器模块
     BrowserModule,
+    // ngModel 需要 引用 Formsmodule 模块
     FormsModule,
-    AppRoutingModule
+    // 路由模块
+    AppRoutingModule,
+    // httpClient 用来发送请求，获取服务端数据
+    HttpClientModule,
+    // web api 一个本地的web服务器
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
