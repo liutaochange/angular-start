a73639d9b73a9d25d9cb392c13bde6c5f6cf886a
liutaochange <liutaochange@163.com>
2018-06-06 19:07:54 +0800

    router

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  private products: Array<Product>
 
   constructor() { }
 
   ngOnInit() {
+    /*this.products = [{
+      new Product(1,'商品名称',2.99,3.55,'还不错',['实战','入门']),
+      new Product(2,'商品名称',2.99,3.55,'还不错',['实战','入门']),
+      new Product(3,'商品名称',2.99,3.55,'还不错',['实战','入门']),
+      new Product(4,'商品名称',2.99,3.55,'还不错',['实战','入门']),
+      new Product(5,'商品名称',2.99,3.55,'还不错',['实战','入门']),
+      new Product(6,'商品名称',2.99,3.55,'还不错',['实战','入门']),
+    }]*/
   }
 
+}
+
+export class Product {
+  id:number;
+  title:string;
+  price:number;
+  rating:number;
+  desc:string;
+  categories: Array<string>;
 }
--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {Hero} from "../hero";
 
 @Component({
   selector: 'app-stars',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stars.component.css']
 })
 export class StarsComponent implements OnInit {
+  @Input() private rating: number
 
+  private stars: boolean[]
   constructor() { }
 
   ngOnInit() {
+    this.starsInit()
   }
-
+  starsInit(): void {
+    this.stars = []
+    for (let i=0;i<5;i++) {
+      this.stars.push(i >= this.rating)
+    }
+  }
 }
